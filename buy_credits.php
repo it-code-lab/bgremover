74f113124535e74f7a50fc4cb86c30ae317c9a9e
numerounodeveloper <numerounodeveloper@gmail.com>
2025-06-14 08:32:27 +0530

    fix

--- buy_credits.php
+++ buy_credits.php
@@ -1,4 +1,14 @@
-<?php
+<head>
+  <meta charset="UTF-8">
+  <title>Register</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="signup_styles.css">
+
+</head>
+
+<body>
+  <?php include("components/header.php"); ?>
+  <?php
 require 'vendor/autoload.php';
 require_once 'db.php';
 session_start();
@@ -7,14 +17,16 @@ session_start();
 
 $session = \Stripe\Checkout\Session::create([
   'payment_method_types' => ['card'],
-  'line_items' => [[
-    'price_data' => [
-      'currency' => 'usd',
-      'product_data' => ['name' => 'Credit Pack (20 images)'],
-      'unit_amount' => 500, // $5
-    ],
-    'quantity' => 1,
-  ]],
+  'line_items' => [
+    [
+      'price_data' => [
+        'currency' => 'usd',
+        'product_data' => ['name' => 'Credit Pack (20 images)'],
+        'unit_amount' => 500, // $5
+      ],
+      'quantity' => 1,
+    ]
+  ],
   'mode' => 'payment',
   'success_url' => 'http://localhost/bgremover/success.php?session_id={CHECKOUT_SESSION_ID}',
   'cancel_url' => 'http://localhost/bgremover/dashboard.php',
@@ -23,3 +35,4 @@ $session = \Stripe\Checkout\Session::create([
 header("Location: " . $session->url);
 exit;
 ?>
+</body>
--- send_reset.php
+++ send_reset.php
@@ -5,21 +5,22 @@
   <link rel="stylesheet" href="signup_styles.css">
 
 </head>
+
 <body>
-    <?php include("components/header.php"); ?>
-<?php
-require_once("db.php");
+  <?php include("components/header.php"); ?>
+  <?php
+  require_once("db.php");
 
-$email = $_POST['email'];
-$token = bin2hex(random_bytes(16));
+  $email = $_POST['email'];
+  $token = bin2hex(random_bytes(16));
 
-$stmt = $pdo->prepare("UPDATE users SET reset_token = ? WHERE email = ?");
-$stmt->execute([$token, $email]);
+  $stmt = $pdo->prepare("UPDATE users SET reset_token = ? WHERE email = ?");
+  $stmt->execute([$token, $email]);
 
-if ($stmt->rowCount() > 0) {
-  echo "Reset link: <a href='reset_password.php?token=$token'>Click here to reset password</a>";
-} else {
-  echo "Email not found.";
-}
-?>
+  if ($stmt->rowCount() > 0) {
+    echo "Reset link: <a href='reset_password.php?token=$token'>Click here to reset password</a>";
+  } else {
+    echo "Email not found.";
+  }
+  ?>
 </body>
